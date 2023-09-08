9fa31d874a6e9c04864e3301b02fc807972a45f5
Jaime Kumar <jaimelacree@gmail.com>
2023-09-08 17:13:01 +0100

    log testing 2

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ async function getBookings() {
 
 app.post('/login', async (req, res) => {
   const {name, pw} = req.body;
-  console.log(name, pw)
+  console.log(name, pw, process.env.ADMINUSER, process.env.ADMINPW)
   if (name !== process.env.ADMINUSER) {
     res.status(500).json({error: 'name'})
   } else if (pw !== process.env.ADMINPW) {
